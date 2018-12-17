d179b6f054a810a4d00588f3f155c9e2e464122c
RubikHouse <ni37fsl@gmail.com>
2018-12-17 18:33:24 +0900

    SnippetDetail에서 put, patch 중복코드 제거

--- app/snippets/views/drf_cbv.py
+++ app/snippets/views/drf_cbv.py
@@ -33,13 +33,17 @@ class SnippetDetail(APIView):
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
+    def put(self, request, pk, format=None, **kwargs):
+        partial = kwargs.pop('partial', False)
         snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
+        serializer = SnippetSerializer(snippet, data=request.data, partial=partial)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        return self.put(request, pk, format, partial=True)
 
     def delete(self, request, pk, format=None):
         snippet = self.get_object(pk)
