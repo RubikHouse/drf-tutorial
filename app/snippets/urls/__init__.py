54bacd55ed11aaa43ce3d1df33b478079056114c
RubikHouse <ni37fsl@gmail.com>
2018-12-17 18:27:25 +0900

    Class-Based View 방식의 APIView 구현

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 
 from . import django_fbv
 from . import drf_fbv
+from . import drf_cbv
 
 urlpatterns = [
     path('django-fbv/', include(django_fbv)),
     path('drf-fbv/', include(drf_fbv)),
+    path('drf-cbv/', include(drf_fbv)),
 ]
